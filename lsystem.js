b49e2ec75c7c72cf8fc355af7eba24e1f4e7f8d6
Brent Fitzgerald <brent.fitzgerald@getbraintree.com>
2015-07-24 18:59:46 -0700

    Add basic L-System API

---
+++ lsystem.js
@@ -0,0 +1,53 @@
+function LSystem(options) {
+  this.axiom = options.axiom || "";
+  this.rules = options.rules || {};
+  this.renderRules = options.renderRules || {};
+}
+
+LSystem.prototype = (function () {
+
+  var state;
+
+  return {
+
+    reset: function () {
+      state = this.axiom;
+
+    },
+
+    render: function () {
+      console.log(state);
+      for (var i = 0; i < state.length; i++) {
+        var s = state.charAt(i);
+        var rule = this.renderRules[s];
+        if (rule) {
+          rule();
+        } else {
+          throw "Unknown instruction " + s;
+        }
+      }
+    },
+
+    step: function () {
+      var newState = "";
+      for (var i = 0; i < state.length; i++) {
+        var s = state.charAt(i);
+        var rule = this.rules[s];
+        if (rule) {
+          newState = newState + rule;
+        } else {
+          newState = newState + s;
+        }
+      }
+      state = newState;
+    },
+
+    simulate: function (n) {
+      for (var i = 0; i < n; i++) {
+        this.step();
+      }
+    }
+  };
+
+}());
+
